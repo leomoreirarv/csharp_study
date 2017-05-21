09c60a640a3b7485b269495c4503d5b5894f0585
leonardomelomoreira <leonardomm@gmail.com>
2017-05-21 21:00:40 +0100

    * .gitignore: first test
    * library.sln:
    * Web.config:
    * Global.asax:
    * library.csproj:
    * Global.asax.cs:
    * packages.config:
    * Web.config:
    * Library.aspx:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * RouteConfig.cs:
    * Error.cshtml:
    * WebApiConfig.cs:
    * Index.cshtml:
    * _Layout.cshtml:
    * HomeController.cs:
    * BookController.cs:
    * LibraryController.cs:

---
+++ library/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace library.Controllers
+{
+	public class BookController : Controller
+	{
+		private string Author;
+		private string Title;
+		private DateTime Year;
+		private int Edition;
+
+		public BookController(string Author, string Title, DateTime Year, int Edition)
+		{
+			this.Author = Author;
+			this.Title = Title;
+			this.Year = Year;
+			this.Edition = Edition;
+		}
+
+		List<string> ShowData()
+		{
+			List<string> ListData = new List<string>();
+			ListData.Add(Author);
+			ListData.Add(Title);
+
+			return ListData;
+		}
+
+	}
+}
---
+++ library/Controllers/LibraryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace library.Controllers
+{
+	public class LibraryController : Controller
+	{
+		public ActionResult Index()
+		{
+			BookController book = new BookController("Luiz de Camões", "Os Lusiadas", new DateTime(1572), 1);
+			ViewData["book"] = book.ViewData;
+			return View();
+		}
+	}
+}
